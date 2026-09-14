56102cea9590e94cc97957855c4d0b5f5c1368a4
Grom <dmitri21777@gmail.com>
2026-09-14 11:40:58 +0300

    1

---
+++ main.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+from ui.app import RadioApp
+
+
+def main():
+
+    root = tk.Tk()
+
+    app = RadioApp(
+        root
+    )
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
